ff7c6732aa72741ec040e91540be061bdf394a1a
Aushiffer <caioshadow2014@gmail.com>
2024-09-02 13:56:53 -0300

    Adds main function guard

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 from queue import Queue
 
-opt = 0
-q = Queue()
+if __name__ == '__main__':
+    opt = 0
+    q = Queue()
 
-while opt != 7:
-    q.print_queue()
+    while opt != 7:
+        q.print_queue()
 
-    print('Select an option: ')
-    print('\n1: enqueue')
-    print('2: dequeue')
-    print('3: size of the queue')
-    print('4: is the queue empty?')
-    print('5: peek head')
-    print('6: peek tail')
-    print('7: quit')
-    
-    opt = int(input())
-    
-    match opt:
-        case 1:
-            val = int(input('\nInput value to enqueue: '))
+        print('Select an option: ')
+        print('\n1: enqueue')
+        print('2: dequeue')
+        print('3: size of the queue')
+        print('4: is the queue empty?')
+        print('5: peek head')
+        print('6: peek tail')
+        print('7: quit')
 
-            q.enqueue(val)
-        case 2:
-            q.dequeue()
-        case 3:
-            print(f'Size: {q.size()}')
-        case 4:
-            print(f'Empty: {q.empty()}')
-        case 5:
-            q.peek_head()
-        case 6:
-            q.peek_tail()
+        opt = int(input())
+
+        match opt:
+            case 1:
+                val = int(input('\nInput value to enqueue: '))
+
+                q.enqueue(val)
+            case 2:
+                q.dequeue()
+            case 3:
+                print(f'Size: {q.size()}')
+            case 4:
+                print(f'Empty: {q.empty()}')
+            case 5:
+                q.peek_head()
+            case 6:
+                q.peek_tail()
